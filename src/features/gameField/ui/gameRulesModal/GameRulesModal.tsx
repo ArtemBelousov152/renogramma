cf515edfd129fbf1921c18dc9bb6c2e32378fd8e
Артем Белоусов <a.belousow@notamedia.com.ru>
2025-08-25 19:31:54 +0300

    0.0.15 Поправил темизацию в модалке с правилами игры

--- src/features/gameField/ui/gameRulesModal/GameRulesModal.tsx
+++ src/features/gameField/ui/gameRulesModal/GameRulesModal.tsx
@@ -9,6 +9,8 @@ import {
 import { useState } from 'react';
 import { Button } from 'shared/components/button';
 
+import classes from './gameRulesModal.module.scss';
+
 export const GameRulesModal = () => {
   const [open, setOpen] = useState(false);
 
@@ -32,56 +34,65 @@ export const GameRulesModal = () => {
         aria-labelledby="rules-dialog-title"
         aria-describedby="rules-dialog-description"
       >
-        <DialogTitle id="rules-dialog-title">
-          Правила и управление игрой
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="rules-dialog-description">
-            <Typography variant="h6">Цель игры</Typography>
-            Нужно построить цепочку из чисел от самого маленького, до самого
-            большого.
-            <br />
-            <br />
-            <Typography variant="h6">Управление</Typography>
-            <ul style={{ paddingLeft: '20px', marginTop: '5px' }}>
-              <li>
-                <strong>Выбор числа:</strong> Левый клик по числу в списке снизу
-                — выбираем его как текущее
-              </li>
-              <li>
-                <strong>Отмена выбора:</strong> Правый клик по любому числу на
-                поле — сбрасываем выбор
-              </li>
-              <li>
-                <strong>Удаление числа:</strong> Правый клик по установленному
-                числу (когда нет выбранного числа)
-              </li>
-              <li>
-                <strong>Взятие числа:</strong> Левый клик по установленному вами
-                числу (когда нет выбранного числа) — удаляет число и делает его
-                текущим
-              </li>
-              <li>
-                <strong>Авто-выбор:</strong> Левый клик по предустановленному
-                числу — автоматически выбирается следующее доступное число
-              </li>
-              <li>
-                <strong>Установка числа:</strong> При наличии текущего числа,
-                левый клик по пустой ячейке — установка текущего числа и
-                автоматический выбор следующего
-              </li>
-              <li>
-                <strong>Замена числа:</strong> При наличии текущего числа, левый
-                клик по установленному вами числу — замена на текущее число
-              </li>
-            </ul>
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Закрыть
-          </Button>
-        </DialogActions>
+        <div className={classes.gameRulesModalTheme}>
+          <DialogTitle id="rules-dialog-title">
+            Правила и управление игрой
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="rules-dialog-description">
+              <Typography className={classes.gameRulesModalTheme} variant="h6">
+                Цель игры
+              </Typography>
+              <span className={classes.gameRulesModalTheme}>
+                Нужно построить цепочку из чисел от самого маленького, до самого
+                большого. При наведении на цепочку, она будет выделена.
+              </span>
+              <br />
+              <br />
+              <Typography className={classes.gameRulesModalTheme} variant="h6">
+                Управление
+              </Typography>
+              <ul className={classes.gameRulesModalList}>
+                <li>
+                  <strong>Выбор числа:</strong> Левый клик по числу в списке
+                  снизу — выбираем его как текущее
+                </li>
+                <li>
+                  <strong>Отмена выбора:</strong> Правый клик по любому числу на
+                  поле — сбрасываем выбор
+                </li>
+                <li>
+                  <strong>Удаление числа:</strong> Правый клик по установленному
+                  числу (когда нет выбранного числа)
+                </li>
+                <li>
+                  <strong>Взятие числа:</strong> Левый клик по установленному
+                  вами числу (когда нет выбранного числа) — удаляет число и
+                  делает его текущим
+                </li>
+                <li>
+                  <strong>Авто-выбор:</strong> Левый клик по предустановленному
+                  числу — автоматически выбирается следующее доступное число
+                </li>
+                <li>
+                  <strong>Установка числа:</strong> При наличии текущего числа,
+                  левый клик по пустой ячейке — установка текущего числа и
+                  автоматический выбор следующего
+                </li>
+                <li>
+                  <strong>Замена числа:</strong> При наличии текущего числа,
+                  левый клик по установленному вами числу — замена на текущее
+                  число
+                </li>
+              </ul>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Закрыть
+            </Button>
+          </DialogActions>
+        </div>
       </Dialog>
     </>
   );
